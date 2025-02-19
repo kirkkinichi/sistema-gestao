815127db0938f5ccfce4a5e0537d41c87ffa3c44
Kirk Silva <kirk.ki@hotmail.com>
2025-02-19 17:36:24 -0300

    montagem da estrutura de dados

---
+++ database/migrations/2025_02_19_203935_create_unidade_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('unidade', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome_fantasia');
+            $table->string('razao_social');
+            $table->string('cnpj');
+            $table->foreignId('bandeira_id')->constrained('bandeira');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('unidade');
+    }
+};
